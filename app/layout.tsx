2053cbba1a9610d52ee3e39e085a00d56cf077a3
tadashijei <tadashijei@gmail.com>
2025-02-04 21:54:03 +0800

    New Service

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Pentaton App',
+  description: 'Pentaton redefines the music industry with decentralized streaming, NFT-backed music, and blockchain-powered royalty distribution, empowering artists and fans alike.',
 }
 
 export default function RootLayout({
